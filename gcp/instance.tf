4e66d877a2a6992495ef762bf75b0dbb704abf91
Andreas Prinz <74512643+andreas-prinz@users.noreply.github.com>
2022-12-31 10:55:14 +0200

    Create instance.tf

---
+++ gcp/instance.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "instance-1"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+  provisioner "local-exec" {
+    command = "echo ${google_compute_instance.vm_instance.name}:  ${google_compute_instance.vm_instance.network_interface[0].access_config[0].nat_ip} >> ip_address.txt"
+  }
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+        nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "terraform-static-ip"
+}
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "bucket-20221231"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on   = [google_storage_bucket.example_bucket]
+#  project      = "<PROJECT_ID>"
+  name         = "instance-2"
+  machine_type = "n1-standard-1"
+#  zone         = "us-west1-c"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+#    network = "default"
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
